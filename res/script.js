752b851a55767e9a75a8b4045f0e38eb50dfdc08
amberlinda <aul123linda@gmail.com>
2022-02-05 13:53:01 +0530

    added code to script

--- res/script.js
+++ res/script.js
@@ -56,19 +56,41 @@ function passwordValidationHandler(ev){
     
 }
 
+const ageValidator = value => {
+    if (!value || value == null){return false}
+    const cont = document.getElementById("age-cont")
+    if(value > 0){
+      cont.classList.remove("input_error")
+      return true
+    }else{    
+        cont.classList.add("input_error")
+      return false
+    }
+}
+
 const submitFormHandler = () => {
     event.preventDefault()
-    let email,phone,password;
+    let email,phone,password,email_val,phone_val,user_val;
     email=phone=password=false;
     const form_elements = document.getElementById("reg-form").elements
     for(let i=0;i<form_elements.length;i++){
         if(form_elements[i].name == "email"){
             email = emailValidator(form_elements[i].value)
+            email_val = form_elements[i].value
         }else if(form_elements[i].name == "phone_number"){
             phone = phoneNumValidator(form_elements[i].value)
+            phone_val = form_elements[i].value
+        }else if(form_elements[i].name == "password"){
+            password = passwordValidator(form_elements[i].value)
+        }else if(form_elements[i].name == "username"){
+            user_val = form_elements[i].value
         }
     }
     if(email && phone && password){
+        document.cookie = `username=${user_val};expires=Mon, 07 Jan 2022 12:00:00 UTC; path=/`
+        document.cookie = `email=${user_val};expires=Mon, 07 Jan 2022 12:00:00 UTC; path=/`
+        localStorage.setItem("phone number",phone_val)
+        localStorage.setItem("email",email_val)
         alert("Form submitted successfully")
     }
 }
